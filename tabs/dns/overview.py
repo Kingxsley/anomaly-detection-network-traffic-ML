d41cd11ed0cdf64a5421cb8b6e08589507ed50ec
Kingxsley <148331450+Kingxsley@users.noreply.github.com>
2025-06-25 04:55:57 +1000

    Update overview.py

--- tabs/dns/overview.py
+++ tabs/dns/overview.py
@@ -1,16 +1,13 @@
 # tabs/dns/overview.py
-
 import streamlit as st
 import pandas as pd
 import plotly.express as px
 from streamlit_autorefresh import st_autorefresh
 from tabs.dns.utils import load_predictions_from_sqlitecloud
 
-def render_overview(time_range, time_range_query_map):  # Rename the function to render_overview
+def render_overview(api_url, influx_measurement, query_duration):
     st_autorefresh(interval=30000, key="overview_refresh")
     st.title("DNS Anomaly Detection Overview")
-    
-    query_duration = time_range_query_map.get(time_range, "-24h")
     df = load_predictions_from_sqlitecloud(time_window=query_duration)
 
     if not df.empty:
